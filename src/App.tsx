3e9055931b4f01cc357d756354c1248e7a7ef706
Aramosgr <aramosgr@gmail.com>
2020-10-02 15:48:42 +0100

    added first text with font and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      SOPA   
+      <h3>Bienvenido/a,</h3> 
+      A estas alturas blabla me da pereza copiar tanto, pasame el texto no la captura sonofabitch
     </div>
   );
 }
