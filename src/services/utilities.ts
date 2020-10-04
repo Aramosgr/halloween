a929ec658edc8ab3ad8c522e8fba49eaa691f25b
Aramosgr <aramosgr@gmail.com>
2020-10-04 11:33:11 +0100

    Fixing the beautiful Cors, set origin in request triggers a preflight

--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -4,12 +4,11 @@ export const gameInstance = axios.create({
   // TODO I should use an env file, fuck Azure
   baseURL: window.location.href.includes("localhost")
     ? ""
-    : "https://halloweenapi.azurewebsites.net/",
-  headers: { "Content-Type": "application/json" },
+    : "https://halloweenapi.azurewebsites.net/"
 });
 
 export const getHeaders = () => {
   return {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json"
   };
 };
